2b4f7d7640aa45735573c3e5ca82bee5c0cc14c9
Olejjka413 <55927593+Olejjka413@users.noreply.github.com>
2021-10-11 20:29:23 +0300

    хедер

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,53 +1,42 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      light
-    >
-    <v-img
-          src="./assets/logo.svg"
+  <v-container>
+    <template>
+	<v-container fluid>
+      <v-app-bar
+      color="red"
+      class="d-flex"
+      dark
+      >
+        <v-img
+          src="../assets/logo.svg"
           height="30"
           width="30"
         >
         </v-img>
-    <v-btn class="ml-9">
+
+        <v-btn class="ml-9" outlined>
           <router-link to="/" class="router-link">Главная</router-link>
         </v-btn>
-        <v-btn class="ml-9">
+        <v-btn class="ml-9" outlined>
           <router-link to="/news" class="router-link">Новости</router-link>
         </v-btn>
-        <v-btn class="ml-9 mr-16">
+        <v-btn class="ml-9 mr-16" outlined>
           <router-link to="/gallery" class="router-link">Галерея</router-link>
         </v-btn>
 
         <v-text-field
-          dense
           label="Поиск"
           :rules="rules"
-          placeholder = "Искать"
           hide-details="auto"
         ></v-text-field>
         <v-icon>mdi-search-web</v-icon>
-    </v-app-bar>
-
-    <v-main>
-      <header/>
-    </v-main>
-  </v-app>
+      </v-app-bar>
+    </v-container>
 </template>
 
-<script>
-
-
-export default {
-  name: 'App',
-
-  components: {
-  },
-
-  data: () => ({
-    //
-  }),
-};
-</script>
+<style scoped>
+  .router-link {
+    text-decoration: none;
+    color: white;
+  }
+</style>
